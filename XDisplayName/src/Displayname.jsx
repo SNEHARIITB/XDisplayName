929a34ff26710c3f7477d53d870971f4e778d6f4
SNEHARIITB <panabakasnehar@gmail.com>
2025-05-08 11:12:56 +0530

    Update Displayname.jsx

--- XDisplayName/src/Displayname.jsx
+++ XDisplayName/src/Displayname.jsx
@@ -28,12 +28,12 @@ export default function DisplayName(){
       <form onSubmit={handleFullName}>
         <h1>Full Name Display</h1>
         <label htmlFor="firstname">First Name : </label>
-        <input type= "text" name="firstname" required
+        <input type= "text" name="firstname" id="firstname" required
           onChange={(e) => handleFirstNameChange(e.target.value)}
         />
         <br />
         <label htmlFor="lastname">Last Name : </label>
-        <input type= "text" name="lastname" 
+        <input type= "text" name="lastname" id="lastname"
             required
            onChange={(e) => handleLastNameChange(e.target.value)}
         />
